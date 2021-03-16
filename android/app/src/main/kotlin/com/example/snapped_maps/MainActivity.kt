5c37e79bc87be401603da8db8cd24f6625199cfe
tokunna <tokunna@gmail.com>
2021-03-16 14:47:46 -0700

    Plugins setup

---
+++ android/app/src/main/kotlin/com/example/snapped_maps/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.snapped_maps
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ios/Runner/AppDelegate.swift
@@ -0,0 +1,14 @@
+import UIKit
+import Flutter
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyCiFeWtk5oKv95CzhZxWOF7vuJ9e8YGN5c")
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
